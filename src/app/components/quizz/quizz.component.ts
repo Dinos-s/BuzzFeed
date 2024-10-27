4b09ce83ca5020d2d6438a6d377be1b6d368122d
Dinos-s <gabriel02ramos@gmail.com>
2024-10-27 11:41:09 -0300

    criando a funcionalidade

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quizz_question from '../../../../public/data/quizz_question.json'
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+  title:string = "";
 
+  questions:any
+  questionsSelected:any
+
+  answers:string[] = []
+  answerSelected:string = ""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
+
+  ngOnInit() {
+    if (quizz_question) {
+      this.finished = false
+      this.title = quizz_question.title
+
+      this.questions = quizz_question.questions
+      this.questionsSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  playChoose(value:string){
+    this.answers.push(value)
+    this.nextStep()
+  }
+
+  nextStep(){
+    this.questionIndex+=1
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionsSelected =  this.questions[this.questionIndex]
+    } else {
+      this.finished = true
+    }
+  }
 }
